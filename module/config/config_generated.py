4e8a0b6a4e0fe44db925ae1c5bdc3fab3891d5b9
雪风大人保佑你 <hanyecheng0907@gmail.com>
2025-10-27 18:29:16 +0800

    舰队强制移动功能nanoda! 只要打开这个功能，大世界每月50万海里轻轻松松nanoda,指挥官再也不用担心找不到明石导致坠机nanoda!

--- module/config/config_generated.py
+++ module/config/config_generated.py
@@ -443,6 +443,7 @@ class GeneratedConfig:
 
     # Group `OpsiHazard1Leveling`
     OpsiHazard1Leveling_TargetZone = 0  # 0, 44, 22
+    OpsiHazard1Leveling_ExecuteFixedPatrolScan = False
 
     # Group `OpsiCheckLeveling`
     OpsiCheckLeveling_TargetLevel = 0
